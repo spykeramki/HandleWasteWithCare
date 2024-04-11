47364202309437e7e2d1823a0bbd4bccb2668939
Rama Krishna Oguri <ramakrishna.oguri@gmail.com>
2024-04-11 19:11:16 +0100

    Added base pointer with direction functionality in UI

--- Assets/Scripts/MapCameraUiCtrl.cs
+++ Assets/Scripts/MapCameraUiCtrl.cs
@@ -6,15 +6,46 @@ public class MapCameraUiCtrl : MonoBehaviour
 {
     public RectTransform playerHead;
 
+    public RectTransform basePointer;
+
+    public Transform baseTransform;
+
     private Transform playerTransform;
+
+    private bool _showPointer = false;
+    private GameObject basePointerGo;
 
     private void Start()
     {
         playerTransform = PlayerCtrl.LocalInstance.GetComponent<Transform>();
+        basePointerGo = basePointer.gameObject;
+        basePointerGo.SetActive(false);
+    }
+
+    private void Update()
+    {
+        ManageIcon(baseTransform, basePointer);
     }
 
     private void LateUpdate()
     {
         playerHead.localRotation =  Quaternion.Euler(0f, 0f, 360-playerTransform.eulerAngles.y);
+    }
+
+    private void ManageIcon(Transform objectTransform, RectTransform iconTransform)
+    {
+        Vector3 baseDirectionFromPlayer = objectTransform.position - playerTransform.position;
+        if (baseDirectionFromPlayer.magnitude > 20f && !_showPointer)
+        {
+            _showPointer = true;
+            iconTransform.gameObject.SetActive(true);
+        }
+        else if (baseDirectionFromPlayer.magnitude <= 20f && _showPointer)
+        {
+            _showPointer = false;
+            iconTransform.gameObject.SetActive(false);
+        }
+        Vector3 basePointerDirection = new Vector3(baseDirectionFromPlayer.x, baseDirectionFromPlayer.z, 0f).normalized;
+        iconTransform.localPosition = (playerHead.localPosition + basePointerDirection * 100f);
     }
 }
